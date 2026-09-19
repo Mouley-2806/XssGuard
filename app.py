3e5ebcf898d0cffa552a172277d63cac93aa5ede
Medusa-28 <mouley2806@gmail.com>
2026-09-19 22:47:19 +0530

    Day 4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response
 
 app = Flask(__name__)
 
@@ -12,10 +12,14 @@ def submit():
     print(username)
     return render_template("result.html", user_input=username)
 
-@app.route("/search", methods=["GET"])
+@app.route("/search")
 def search():
     query = request.args.get("q")
-    return f"You are searching for: {query}"
+    print("Query parameter:", query)
+    response = make_response(f"You searched for: {query}")
+    response.headers["X-XSSGuard-Test"] = "Day4"
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
