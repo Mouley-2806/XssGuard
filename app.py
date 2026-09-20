1f971650af7bda9728edd8e676f8d9101f7ce3a4
Medusa-28 <mouley2806@gmail.com>
2026-09-20 23:35:49 +0530

    Day 5 plain cookie and session cookie

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request,make_response
+from flask import Flask, render_template, request,make_response,session
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  
 
 @app.route("/")
 def home():
@@ -19,6 +20,22 @@ def search():
     response = make_response(f"You searched for: {query}")
     response.headers["X-XSSGuard-Test"] = "Day4"
     return response
+
+@app.route("/set_cookie")
+def set_cookie():
+    response=make_response("Cookie has been set")
+    response.set_cookie("Username", "Day4")
+    return response
+@app.route("/set_session")
+def set_session():
+    session["username"]="Mouley"
+    return "session has been set"
+
+@app.route("/get_session")
+def get_cookie():
+    username=session.get("username")
+    return f"Session username: {username}"
+    
 
 
 if __name__ == "__main__":
