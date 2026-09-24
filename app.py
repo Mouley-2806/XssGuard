b52bc8696c23b8dd32bf3ec1c9bdec67a8eaadb4
Medusa-28 <mouley2806@gmail.com>
2026-09-24 23:55:24 +0530

    ..

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def submit():
 
 @app.route("/search")
 def search():
-    query = request.args.get("q")
+    query = request.args.get("r")
     print("Query parameter:", query)
     response = make_response(f"You searched for: {query}")
     response.headers["X-XSSGuard-Test"] = "Name"
